dec9f23687a3603731b5e2962f10360bd8100764
YueJeong <sunny819819@naver.com>
2021-06-16 17:56:59 +0900

    로그인, 회원가입 서버-프론트 연결

--- app/src/main/java/com/example/dbproject/LoginActivity.java
+++ app/src/main/java/com/example/dbproject/LoginActivity.java
@@ -9,7 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -41,24 +43,24 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // EditText에 현재 입력되어있는 값을 get(가져온다)해온다.
                 String userName = et_name.getText().toString();
-                String userNickname = et_nickname.getText().toString();
+                String nickName = et_nickname.getText().toString();
 
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
-                            // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
-                            System.out.println("hongchul" + response);
                             JSONObject jsonObject = new JSONObject(response);
                             boolean success = jsonObject.getBoolean("success");
+
                             if (success) { // 로그인에 성공한 경우
-                                String userID = jsonObject.getString("userID");
-                                String userPass = jsonObject.getString("userPassword");
+                                String userName= jsonObject.getString("userName");
+                                String nickName = jsonObject.getString("nickName");
 
                                 Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                intent.putExtra("userID", userID);
-                                intent.putExtra("userPass", userPass);
+
+                                intent.putExtra("userName", userName);
+                                intent.putExtra("nickName", nickName);
                                 startActivity(intent);
                             } else { // 로그인에 실패한 경우
                                 Toast.makeText(getApplicationContext(),"로그인에 실패하였습니다.",Toast.LENGTH_SHORT).show();
@@ -69,11 +71,11 @@ public class LoginActivity extends AppCompatActivity {
                         }
                     }
                 };
-                /*
-                LoginRequest loginRequest = new LoginRequest(userID, userPass, responseListener);
+
+                LoginRequest loginRequest = new LoginRequest(userName, nickName, responseListener);
                 RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
                 queue.add(loginRequest);
-                 */
+
             }
         });
     }
