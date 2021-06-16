1eace987c90fe46c7a9276ebeff124d2e1868e72
kinglim00 <seohee0012@gmail.com>
2021-06-17 02:21:02 +0900

    Add my_module [Mul]

---
+++ app/src/main/java/com/example/dbproject/ProductActivity3.java
@@ -0,0 +1,37 @@
+package com.example.dbproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+// MainActivity는 상품 소개 창
+public class ProductActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button purchase_btn = (Button) findViewById(R.id.purshase_btn);
+        purchase_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PurchaseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 장바구니 창을 따로 만들지 문제라서 일단 보류
+        Button cart_btn = (Button) findViewById(R.id.cart_btn);
+        cart_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CartActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
